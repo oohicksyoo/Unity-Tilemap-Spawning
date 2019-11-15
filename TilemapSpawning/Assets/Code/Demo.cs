1d1a13bd5c1806d29fe7cc8b8df4a814c5456fc5
Alex Hicks <alexhicks@live.ca>
2019-11-14 21:33:11 -0500

    Init push

---
+++ TilemapSpawning/Assets/Code/Demo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Project {
+    public class Demo : MonoBehaviour {
+
+        [SerializeField]
+        private Tilemap tilemap;
+
+        [SerializeField]
+        private Tile spawnTile;
+
+        public void Start() {
+            
+        }
+
+        public void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                tilemap.SetTile(new Vector3Int(), spawnTile);
+            }
+        }
+    }
+}
